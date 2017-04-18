7f69f98536e387defbc8b96a8267f9450a142ff5
morfowt <san67@pitt.edu>
2017-04-17 23:02:06 -0400

    Update Gallery.java

--- app/src/main/java/edu/pitt/cs/cs1635/jah234/cathedraltourguide/Gallery.java
+++ app/src/main/java/edu/pitt/cs/cs1635/jah234/cathedraltourguide/Gallery.java
@@ -181,6 +181,10 @@ public class Gallery extends Fragment {
                 downOrder = (direction.getSelectedItemPosition() == 0);
                 orderResults();
                 adapter.notifyDataSetChanged();
+                SharedPreferences.Editor editor = keyPair.edit();
+                editor.putInt("CategorySpinner", category.getSelectedItemPosition());
+                editor.putInt("DirectionSpinner", direction.getSelectedItemPosition());
+                editor.commit();
             }
         });
 
@@ -282,14 +286,5 @@ public class Gallery extends Fragment {
         public int compare(GalleryCardInfo e1, GalleryCardInfo e2) {
             return e1.compare(e2);
         }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        SharedPreferences.Editor editor = keyPair.edit();
-        editor.putInt("CategorySpinner", category.getSelectedItemPosition());
-        editor.putInt("DirectionSpinner", direction.getSelectedItemPosition());
-        editor.commit();
     }
 }
